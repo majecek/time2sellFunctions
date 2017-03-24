a65fbe7adcd7a8d55e47cb6ec6ed871bb4e9eaa8
Marek Chowaniok <marek.chowaniok@gmail.com>
2017-03-23 23:01:33 -0600

    notification by email when treshold is hit

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,37 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const _ = require('lodash');
-const googleStocks = require('google-stocks');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const _ = require('lodash')
+const googleStocks = require('google-stocks')
+const nodemailer = require('nodemailer')
 
+const gmailEmail = encodeURIComponent(functions.config().gmail.email)
+const gmailPassword = encodeURIComponent(functions.config().gmail.password)
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`)
 
-admin.initializeApp(functions.config().firebase);
-const db = admin.database();
+admin.initializeApp(functions.config().firebase)
+const db = admin.database()
 
-function convertToArray(objGroup) {
+exports.updateStocks = functions.https.onRequest((req, res) => {
+
+  db.ref('users').once('value', snapUser => {
+    // console.log('snapUser',snapUser.val());
+
+    _.keys(snapUser.val()).forEach(userKey => {
+      const userObj = snapUser.val()[userKey]
+      const userEmail = userObj.email
+      // console.log('===>USER:', userKey)
+      db.ref('users').child(`${userKey}/stocks`).once('value', snapStocks => {
+        getPricesFromGoogle(snapStocks, userKey, userEmail)
+      })
+      // console.log('===>USER-END:', userKey);
+
+    })
+  })
+
+  res.send('Hello from Firebase!')
+})
+
+function convertToArray (objGroup) {
   const newArray = []
   if (!objGroup) {
     return newArray
@@ -19,71 +43,78 @@ function convertToArray(objGroup) {
   return newArray
 }
 
-function preparePricesForDB(stocksFromDB, googlePrices, userKey) {
+function preparePricesForDB (stocksFromDB, googlePrices, userKey, userEmail) {
   let result = {}
   stocksFromDB.map(stock => {
     const updatedStockFromGoogle = googlePrices.filter(googlePrice => {
       return googlePrice.t === stock.symbol
-    });
-console.log('Prices from Google', userKey, JSON.stringify(updatedStockFromGoogle));
-    const lastPrice = updatedStockFromGoogle[0] ? updatedStockFromGoogle[0].l : 1;
-    const highestPrice = stock.lastPrice > stock.highestPrice ? stock.lastPrice : stock.highestPrice;
-    const gain =  _.round(((100 * stock.lastPrice ) / stock.purchasePrice) - 100, 2)
-    const currentTreshold = _.round(((1 - (stock.lastPrice / highestPrice)) * 100),2)
-console.log(userKey,'highestPrice', highestPrice, 'lastPrice', lastPrice, 'gain', gain, 'currentTreshold', currentTreshold)
-    result[`stocks/${stock.id}/lastPriceTimeStamp`] = new Date().toISOString();
-    result[`stocks/${stock.id}/lastPrice`] = lastPrice;
+    })
+    console.log('Prices from Google', userKey, JSON.stringify(updatedStockFromGoogle))
+    const lastPrice = updatedStockFromGoogle[0] ? updatedStockFromGoogle[0].l : 1
+    const highestPrice = stock.lastPrice > stock.highestPrice ? stock.lastPrice : stock.highestPrice
+    const gain = _.round(((100 * lastPrice ) / stock.purchasePrice) - 100, 2)
+    const currentTreshold = _.round(((1 - (lastPrice / highestPrice)) * 100), 2)
+    console.log(userKey, 'highestPrice', highestPrice, 'lastPrice', lastPrice, 'gain', gain, 'currentTreshold', currentTreshold)
+    result[`stocks/${stock.id}/lastPriceTimeStamp`] = new Date().toISOString()
+    result[`stocks/${stock.id}/lastPrice`] = lastPrice
     result[`stocks/${stock.id}/highestPrice`] = highestPrice
     result[`stocks/${stock.id}/gain`] = gain
     result[`stocks/${stock.id}/currentTreshold`] = currentTreshold
+
+    if (currentTreshold > stock.threshold && !stock.notified) {
+      sendEmail(userEmail, stock, gain, highestPrice, lastPrice)
+      result[`stocks/${stock.id}/notified`] = true
+    }
   })
-  return result;
+  return result
 }
 
-function getPricesFromGoogle(snapStocks, userKey) {
+function sendEmail (email, stock, gain, highestPrice, lastPrice) {
+  const mailOptions = {
+    from: '"Time2Sell Corp." <noreply@firebase.com>',
+    to: email,
+    subject: 'Time2Sell Treshold Alert',
+    text: `Hello
+    you just hit your treshold alert! Here is summary:
+    
+    symbol: ${stock.symbol} 
+    highest price: ${highestPrice} - current price: ${lastPrice}
+    current P&L: ${gain}
+    
+    regards, time2sell
+    `
+  }
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('New subscription confirmation email sent to:', email)
+    })
+}
 
-  const stocksFromDB = convertToArray(snapStocks.val());
+function getPricesFromGoogle (snapStocks, userKey, userEmail) {
+
+  const stocksFromDB = convertToArray(snapStocks.val())
   const stocksSymbols = stocksFromDB.map(stock => {
     return stock.symbol
-  });
+  })
   // console.log('stocksArray', userKey, stocksFromDB);
-  console.log('stocksSymbols', userKey, stocksSymbols);
+  console.log('stocksSymbols', userKey, stocksSymbols)
 
   googleStocks([stocksSymbols])
     .then(function (data) {
-      const result = preparePricesForDB(stocksFromDB, data, userKey)
-      db.ref(`users/${userKey}`).update(result);
-      console.log('stocks updated', userKey, result);
+      const result = preparePricesForDB(stocksFromDB, data, userKey, userEmail)
+      db.ref(`users/${userKey}`).update(result)
+      console.log('stocks updated', userKey, result)
     })
     .catch(function (error) {
-      console.error('google-error', userKey, stocksSymbols, error);
+      console.error('google-error', userKey, stocksSymbols, error)
       // if (error.code !== 'ECONNRESET') {
-        db.ref(`users/${userKey}/errors`).push().set({
-          error,
-          errorTimeStamp: new Date().toISOString()
-        });
+      db.ref(`users/${userKey}/errors`).push().set({
+        error,
+        errorTimeStamp: new Date().toISOString()
+      })
       // }
 
-    });
+    })
 }
-
-
-exports.updateStocks = functions.https.onRequest((req, res) => {
-
-  db.ref('users').once('value', snapUser => {
-    _.keys(snapUser.val()).forEach(userKey => {
-
-      // console.log('===>USER:', userKey);
-      db.ref('users').child(`${userKey}/stocks`).once('value', snapStocks => {
-        getPricesFromGoogle(snapStocks, userKey);
-      });
-      // console.log('===>USER-END:', userKey);
-
-    });
-  });
-
-  res.send("Hello from Firebase!");
-});
 
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
